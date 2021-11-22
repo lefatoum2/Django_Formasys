fedf832b09f732f3be4440a4fed2deabb5c41d5c
christian29200 <lefatoum@gmail.com>
2021-11-22 23:22:53 +0100

    Réaliser une Application Django - Partie 6-: Les relations one to many et many to many

--- projet/commande/models.py
+++ projet/commande/models.py
@@ -1,12 +1,16 @@
+import sys
 from django.db import models
+from produit.models import Produit
+from client.models import Client
+
 
 # Create your models here.
 class Commande(models.Model):
     STATUS = (('en instance', 'en instance'), ('non livré', 'non livré'), ('livré', 'livré"'))
-    # client =
-    # produit =
+    client = models.ForeignKey(Client, null=True, on_delete=models.SET_NULL)
+    produit = models.ForeignKey(Produit, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=200, null=True)
-    date_creation = models.DateTimeField(auto_now_add=True)
+    date_creation = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.nom
--- projet/produit/admin.py
+++ projet/produit/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Produit
+from .models import Produit, Tag
 # Register your models here.
 
 admin.site.register(Produit)
+admin.site.register(Tag)
